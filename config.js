478c1b681178aa6049fe2fe45cea25cb93cdb179
Steve Saxton <steves@codeuniquely.co.uk>
2017-08-07 05:07:31 +0100

    re-organisation

--- config.js
+++ config.js
@@ -6,10 +6,18 @@ module.exports = {
     storageToUse: 'session',  // 'session' or 'local'
   },
 
+  locale: 'en-GB',
+
   server: {
+    offline: true,
+
     port: 5000,
-    apiRoot: 'http://localhost:8000/api/v1',
     portalRoot: 'http://localhost:5000',
+
+    apiRoot: 'http://localhost:8000/api/v1',
   },
+
+  // mock: {
+  // }
 
 };
