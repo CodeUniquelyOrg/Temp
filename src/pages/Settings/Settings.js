61a285cea48038962702cce68777b01cecbb062b
Steve Saxton <steves@codeuniquely.co.uk>
2017-08-09 17:16:55 +0100

    fixes

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -15,8 +15,9 @@ import Translate from 'components/Translate';
 //
 
 import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
+import { Tabs, Tab } from 'material-ui/Tabs';
+import FontIcon from 'material-ui/FontIcon';
 import TextField from 'material-ui/TextField';
-// import Typography from 'material-ui/Typography';
 
 import Navigation from 'components/Navigation';
 import style from './style.pcss';
@@ -67,6 +68,12 @@ const Form = reduxForm({
   validate,           // validate
   // warn
 });
+
+const overrides = {
+  tabBar: {
+    backgroundColor: 'white',
+  }
+};
 
 const Settings = class Settings extends Component {
 
@@ -122,57 +129,78 @@ const Settings = class Settings extends Component {
 
         <form className={`${style.form} ${style.center}`} onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
 
-          <h1>
-            <Translate id="myAccount" />
-          </h1>
-          <br/>
-          <br/>
+          <Tabs style={overrides.tabBar}>
 
-          <p>
-            Lorem ipsum dolor sit amet, no populo causae mei, pro an quem quot invidunt,
-            ei delicata pericula cum. Dolore populo disputationi ius at, virtute elaboraret
-            scriptorem id nec. Vulputate maiestatis eu mel, magna zril ex vix. Dicam
-            menandri deseruisse ei mel.
-          </p>
-          <br/>
-          <br/>
+            <Tab
+              icon={<FontIcon className="material-icons">drive_eta</FontIcon>}
+              label="MAIN"
+            />
 
-          <h2><Translate id="preferences" /></h2>
-          <br/>
+            <Tab
+              icon={<FontIcon className="material-icons">settings</FontIcon>}
+              label="SETTINGS"
+            >
+              <br/>
+              <br/>
+              <h1>
+                <Translate id="myAccount" />
+              </h1>
+              <br/>
+              <br/>
 
-          <h3><Translate id="pressureUnits" /></h3>
-          <br/>
-          <br/>
-          <RadioButtonGroup  name="pressure" defaultSelected="kPa">
-            <RadioButton value="kPa" label="kPa" />
-            <RadioButton value="bar" label="bar" />
-            <RadioButton value="PSI" label="PSI" />
-          </RadioButtonGroup>
+              <p>
+                Lorem ipsum dolor sit amet, no populo causae mei, pro an quem quot invidunt,
+                ei delicata pericula cum. Dolore populo disputationi ius at, virtute elaboraret
+                scriptorem id nec. Vulputate maiestatis eu mel, magna zril ex vix. Dicam
+                menandri deseruisse ei mel.
+              </p>
+              <br/>
+              <br/>
 
-          <h3><Translate id="depthUnits" /></h3>
-          <br/>
-          <br/>
-          <RadioButtonGroup  name="depth"  defaultSelected="mm">
-            <RadioButton value="mm" label="mm" />
-            <RadioButton value="1/32" label='1/32"' />
-          </RadioButtonGroup>
+              <h2><Translate id="preferences" /></h2>
+              <br/>
 
-          <h3><Translate id="personal" /></h3>
-          <div>
-            <Field name="greeting" label="greeting" component={renderField} />
-          </div>
-          <div>
-            <Field name="forename" label="forename" component={renderField} />
-          </div>
-          <div>
-            <Field name="surname" label="surname" component={renderField} />
-          </div>
+              <h3><Translate id="pressureUnits" /></h3>
+              <br/>
+              <br/>
+              <RadioButtonGroup  name="pressure" defaultSelected="kPa">
+                <RadioButton value="kPa" label="kPa" />
+                <RadioButton value="bar" label="bar" />
+                <RadioButton value="PSI" label="PSI" />
+              </RadioButtonGroup>
 
-          <h3 className={style.subHeading}><Translate id="registrations" /></h3>
-          <p>
-            <mark>Some way to choose from the registrations</mark>
-            <mark>and edit tyre pressures for them / delete</mark>
-          </p>
+              <h3><Translate id="depthUnits" /></h3>
+              <br/>
+              <br/>
+              <RadioButtonGroup  name="depth"  defaultSelected="mm">
+                <RadioButton value="mm" label="mm" />
+                <RadioButton value="1/32" label='1/32"' />
+              </RadioButtonGroup>
+
+              <h3><Translate id="personal" /></h3>
+              <Field name="greeting" label="greeting" component={renderField} />
+              <br/>
+              <Field name="forename" label="forename" component={renderField} />
+              <br/>
+              <Field name="surname" label="surname" component={renderField} />
+              <br/>
+
+              <h3 className={style.subHeading}><Translate id="registrations" /></h3>
+              <p>
+                <mark>Some way to choose from the registrations</mark>
+                <mark>and edit tyre pressures for them / delete</mark>
+              </p>
+
+            </Tab>
+            <Tab
+              icon={<FontIcon className="material-icons">history</FontIcon>}
+              label="RESULTS"
+            />
+            <Tab
+              icon={<FontIcon className="material-icons">help</FontIcon>}
+              label="HELP"
+            />
+          </Tabs>
 
         </form>
 
