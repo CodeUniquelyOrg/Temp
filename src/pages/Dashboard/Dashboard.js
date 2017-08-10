689c0fc7e3dceccbc0ad048eaac50da9b356b97b
Steve Saxton <steves@codeuniquely.co.uk>
2017-08-10 06:24:52 +0100

    refactored settings into component

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -9,14 +9,18 @@ import { getUserData } from 'actions/user';
 // import HeaderBar from 'components/HeaderBar';   // eslint-disable-line no-unused-vars
 // import FooterBar from 'components/FooterBar';   // eslint-disable-line no-unused-vars
 
+import { Tabs, Tab } from 'material-ui/Tabs';
+import FontIcon from 'material-ui/FontIcon';
+
 // =====================================
 // UI Styling and other stuff ike that
 // =====================================
-import Navigation from 'components/Navigation';
+// import Navigation from 'components/Navigation';
 // import Plate from 'components/Plate';
 // import Tyre from 'components/Tyre';
 // import car from 'img/car.png';
 import Car from 'components/Car';
+import Settings from 'components/Settings';
 
 // Styling
 import style from './style.pcss';
@@ -34,13 +38,13 @@ const regNum = 'L5 MNE';
 //
 // *** WILl NEED TO use the TRANSLATE OBJECT HERE ***
 //
-const getNavigationItems = () => {
-  return [
-    { label: 'me',       icon: 'person',    path: '/settings' },
-    { label: 'results',  icon: 'history',   path: '/history'  },
-    { label: 'help',     icon: 'help',      path: '/faq'      },
-  ];
-};
+// const getNavigationItems = () => {
+//   return [
+//     { label: 'me',       icon: 'person',    path: '/settings' },
+//     { label: 'results',  icon: 'history',   path: '/history'  },
+//     { label: 'help',     icon: 'help',      path: '/faq'      },
+//   ];
+// };
 
 const Dashboard = class Dashboard extends Component {
 
@@ -64,8 +68,7 @@ const Dashboard = class Dashboard extends Component {
     }
   }
 
-  render() {
-
+  renderCar() {
     const {
       user,
       tyres,
@@ -97,18 +100,66 @@ const Dashboard = class Dashboard extends Component {
         }
       }
     }
+    return car;
+  }
+
+  renderSettings() {
+    const {
+      user,
+    } = this.props;
+
+    let settings;
+    if(user) {
+      settings = (
+        <Settings user={user} />
+      );
+    }
+    return settings;
+  }
+
+  render() {
+
+    const {
+      user,
+      tyres,
+    } = this.props;
 
     return (
       <div className={style.root}>
-
-        <Navigation items={getNavigationItems()} />
 
         <div>
           {this.renderAlert()}
         </div>
 
-        <h1>Your Latest Results</h1>
-        {car}
+        <Tabs>
+
+          <Tab
+            icon={<FontIcon className="material-icons">drive_eta</FontIcon>}
+            label="MAIN"
+          >
+            {this.renderCar()}
+          </Tab>
+
+          <Tab
+            icon={<FontIcon className="material-icons">history</FontIcon>}
+            label="RESULTS"
+          >
+          </Tab>
+
+          <Tab
+            icon={<FontIcon className="material-icons">settings</FontIcon>}
+            label="SETTINGS"
+          >
+            {this.renderSettings()}
+          </Tab>
+
+          <Tab
+            icon={<FontIcon className="material-icons">help</FontIcon>}
+            label="HELP"
+          >
+          </Tab>
+
+        </Tabs>
 
       </div>
     );
